a2d215422a7643f0e43b3b3bc4df3acf8db5accc
Isadora Gonçalves <isadora.goncalves@estudante.ufjf.br>
2023-05-26 23:56:35 -0300

    Feat: Adiciona componentes básicos

---
+++ src/components/tools/Tools.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <CardDealerTool />
+
+    <hr />
+
+    <DiceRollerTool />
+  </section>
+</template>
+
+<script setup lang="ts">
+  import DiceRollerTool from "./DiceRollerTool.vue";
+  import CardDealerTool from "./CardDealerTool.vue";
+</script>
+
+<style scoped>
+
+hr {
+  border: .5px solid white;
+  height: 0;
+  margin: 2em 0;
+}
+
+</style>
